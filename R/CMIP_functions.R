e62fca3e49e595a6bc80a94fd6075a5b933149a9
dinilu <dnietolugilde@gmail.com>
2022-07-19 13:56:56 +0200

    Change package name and add documentation

--- R/CMIP_functions.R
+++ R/CMIP_functions.R
@@ -1,31 +1,31 @@
 
 #' Load CMIP5 data into climate4R grid format
 #'
-#' @param var_list TBW
-#' @param var_new_list TBW
-#' @param indir TBW
-#' @param rcp TBW
-#' @param mod TBW
-#' @param lonLim TBW
-#' @param latLim TBW
-#' @param years TBW
-#' @param dictionary TBW
+#' @param var_list A character vector with names of the CMIP5 variables in their original format (as coded in original CMIP5 datasets).
+#' @param var_new_list A character vector with names of the CMIP5 variables in their standardized format (as they should match those from other datasets, e.g. TraCE21ka and UERRA datasets).
+#' @param indir A character string with the directory path for CMIP5 data. This directory should have a folder with the name of the RCP that is going to be loaded. The RCP folder should have in turn a folder with the name of the General Circulation Model that is goint to be loaded.
+#' @param rcp A character vector with the name of the RCP that is going to be loaded.
+#' @param mod A character vector with the name of the GCM that is going to be loaded.
+#' @param lonLim A numeric vector (length = 2) with the longitudinal extent that is going to be loaded.
+#' @param latLim A numeric vector (length = 2) with the latitudinal extent that is going to be loaded.
+#' @param years A numeric vector with the sequence of years that are going to be loaded.
+#' @param dictionary A data dictionary (as in \code{\link[loadeR]{loadGridData}}) that is going to be used to load the data. By default it uses an internal dictionary that standardize CMIP5, TraCE21ka, and UERRA data to the same format.
 #'
-#' @return TBW
+#' @return A list object with the structure suitable for the climate4R suite. More specifically the data are as returned from the \code{\link[loadeR]{loadGridData}} function.
 #' @export
 #'
-#' @examples #TBW
-loadCMIP <- function(var_list, var_new_list, indir = "../../Data/CMIP5/", rcp, mod, lonLim = NULL, latLim = NULL, years = NULL, dictionary = system.file("extdata", "CMIP5_dictionary.csv", package = "TraCE21kaDSR")){
-  # var_list <- cmip5.vars
-  # var_new_list <- cmip5.new.vars
-  # indir <- "../../Data/CMIP5/"
-  # rcp <- "rcp2.6"
-  # mod <- cmip5.mods[[1]]
-  # lonLim <- cmip5.lon
-  # latLim <- cmip5.lat
-  # years <- 2006:2100
-  # dictionary <- system.file("extdata", "CMIP5_dictionary.csv", package = "TraCE21kaDSR")
-  
+#' @examples
+#' \dontrun{
+#' dsclim::loadCMIP("tas",
+#'                  "tas",
+#'                  "data/",
+#'                  "rcp2.6",
+#'                  "CESM1-CAM5",
+#'                  c(-11.25, 12.50),
+#'                  c(27, 45),
+#'                  2006:2100)
+#' }
+loadCMIP <- function(var_list, var_new_list, indir, rcp, mod, lonLim = NULL, latLim = NULL, years = NULL, dictionary = system.file("extdata", "CMIP5_dictionary.csv", package = "dsclim")){
   if(rcp != "historical"){
     rcp_folder <- paste0("RCP_", substr(rcp, 4, 4), "_", substr(rcp, 6, 6))
     rcp_file <- paste0("rcp", substr(rcp, 4, 4), substr(rcp, 6, 6))
@@ -34,27 +34,27 @@ loadCMIP <- function(var_list, var_new_list, indir = "../../Data/CMIP5/", rcp, m
     rcp_folder <- rcp
     rcp_file <- rcp
     dates <- "185001-200512"
-  } 
-  
+  }
+
   file_list <- paste0(indir, rcp, "/", mod, "_", rcp_folder, "/", var_list, "_Amon_", mod, "_", rcp_file, "_r1i1p1_", dates, ".nc")
-  
+
   data <- mapply(loadeR::loadGridData, dataset = file_list, var = var_new_list, MoreArgs=list(lonLim = lonLim, latLim = latLim, dictionary = dictionary), SIMPLIFY = FALSE)
-  
+
   start_date <- paste0(years[[1]], "-01-01")
   end_date <- paste0(years[[length(years)]], "-12-31")
-  
+
   data <- lapply(data, FUN = modifyDates, start_date, end_date)
-  
+
   names(data) <- var_new_list
-  
+
   data <- transformeR::makeMultiGrid(data)
-  
+
   if(!is.null(years)){
     data <- transformeR::subsetGrid(data, years = years)
-  } 
-  
+  }
+
   return(data)
-} 
+}
 
 
 
@@ -80,8 +80,8 @@ loadCMIP <- function(var_list, var_new_list, indir = "../../Data/CMIP5/", rcp, m
 #' @export
 #'
 #' @examples #TBW
-downscaleCMIP5 <- function(uerra, var_list, var_new_list, rcp, mod, lonLim = cmip5.lon, latLim = cmip5.lat, dictionary = system.file("extdata", "CMIP5_dictionary.csv", package = "TraCE21kaDSR"), indir = "../../Data/CMIP5/", outdir = "../../Output/CMIP5/", local.var, spatial.pars = cmip5.spatial.pars, method = "GLM", family.link = family.link, global.nc.attributes = global.nc.attributes){
-  
+downscaleCMIP5 <- function(uerra, var_list, var_new_list, rcp, mod, lonLim = cmip5.lon, latLim = cmip5.lat, dictionary = system.file("extdata", "CMIP5_dictionary.csv", package = "dsclim"), indir, outdir = "Output/CMIP5/", local.var, spatial.pars = cmip5.spatial.pars, method = "GLM", family.link = family.link, global.nc.attributes = global.nc.attributes){
+
   # uerra <- uerra
   # var_list <- cmip5.vars
   # var_new_list <- cmip5.new.vars
@@ -89,61 +89,61 @@ downscaleCMIP5 <- function(uerra, var_list, var_new_list, rcp, mod, lonLim = cmi
   # mod <- cmip5.mods[[3]]
   # lonLim <- cmip5.lon
   # latLim <- cmip5.lat
-  # dictionary <- system.file("extdata", "CMIP5_dictionary.csv", package = "TraCE21kaDSR")
-  # indir <- "../../Data/CMIP5/"
-  # outdir <- "../../Output/CMIP5/"
+  # dictionary <- system.file("extdata", "CMIP5_dictionary.csv", package = "dsclim")
+  # indir <- "Data/CMIP5/"
+  # outdir <- "Output/CMIP5/"
   # local.var <- "cld"
   # spatial.pars = cmip5.spatial.pars
   # family.link <- "gaussian"
   # global.nc.attributes <- global.nc.attributes
-  
+
   if(!dir.exists(paste0(outdir, rcp, "/", mod, "/", local.var, "/dat"))){
     dir.create(paste0(outdir, rcp, "/", mod, "/", local.var, "/dat"), recursive = TRUE)
-  } 
-  
+  }
+
   hist.cmip5 <- loadCMIP(var_list = var_list, var_new_list = var_new_list, indir = indir, rcp = "historical", mod = mod, years=1961:1990)
-  
+
   if(local.var == "pr"){
     uerra.bin <- transformeR::binaryGrid(uerra, condition = "GE", threshold = 1)
-    
+
     data.bin <- downscaleR::prepareData(hist.cmip5, uerra.bin, spatial.predictors = cmip5.spatial.pars)
     data <- downscaleR::prepareData(hist.cmip5, uerra, spatial.predictors = cmip5.spatial.pars)
-    
+
     model.bin <- downscaleR::downscaleTrain(data.bin, method = "GLM", family = stats::binomial(link="logit"), predict = TRUE)
     model <- downscaleR::downscaleTrain(data, method = "GLM", family = family.link, predict = TRUE, condition = "GE", threshold = 1)
   } else {
     data <- downscaleR::prepareData(hist.cmip5, uerra, spatial.predictors = cmip5.spatial.pars)
     model <- downscaleR::downscaleTrain(data, method = method, family = family.link, predict = TRUE)
-  } 
-  
+  }
+
   rcp.cmip5.1 <- loadCMIP(var_list = var_list, var_new_list = var_new_list, indir = indir, rcp = "historical", mod = mod, years=1991:2005)
   rcp.cmip5.2 <- loadCMIP(var_list = var_list, var_new_list = var_new_list, indir = indir, rcp = rcp, mod = mod, years = 2006:2100)
-  
+
   rcp.cmip5 <- transformeR::bindGrid(rcp.cmip5.1, rcp.cmip5.2, dimension = "time")
-  
+
   new.data <- downscaleR::prepareNewData(rcp.cmip5, data)
-  
+
   if( local.var == "pr"){
     pred.bin <- downscaleR::downscalePredict(new.data, model.bin)
     pred.cont <- downscaleR::downscalePredict(new.data, model)
     pred <- transformeR::gridArithmetics(pred.bin, pred.cont, operator = "*")
   } else {
     pred <- downscaleR::downscalePredict(new.data, model)
-  } 
-  
+  }
+
   pred$Data <- round(pred$Data, 2)
-  
+
   loadeR.2nc::grid2nc(pred, NetCDFOutFile = paste0(outdir, rcp, "/", mod, "/", local.var, "/", local.var, "1991-2100.nc"), missval = -9999, globalAttributes = global.nc.attributes)
-  
+
   for(i in 1:110){
-    y <- c(1991:2100)[i] 
-    yBP <- c(41:151)[i] 
+    y <- c(1991:2100)[i]
+    yBP <- c(41:151)[i]
     pred.i <- transformeR::subsetGrid(pred, years=y)
-    
+
     pred.df <- nc2sp_df(pred.i)
-    
+
     file.name <- paste0(outdir, rcp, "/", mod, "/", local.var, "/dat/", local.var, yBP, ".dat")
     utils::write.table(pred.df, file = file.name, sep = "\t")
-  } 
+  }
   return("Done")
-} 
+}
--- R/TraCE21ka_functions.R
+++ R/TraCE21ka_functions.R
@@ -2,7 +2,7 @@
 #' Title
 #'
 #' @param indir TBW
-#' @param files_n TBW 
+#' @param files_n TBW
 #' @param vars TBW
 #'
 #' @return TBW
@@ -12,16 +12,16 @@
 #' # historical.files <- traceFileNames("Data/TraCE21ka/", 36)
 #' # LGM.files <- traceFileNames("Data/TraCE21ka/", 1)
 traceFileNames <- function(indir, files_n = 36, vars = NULL){
-  # indir <- "../../Data/TraCE21ka/"
+  # indir <- "Data/TraCE21ka/"
   if(is.null(vars)){
     vars <- trace.source.var.names
   }
-  years.n <- trace.years.n[files_n] 
+  years.n <- trace.years.n[files_n]
   years.bp <- trace.years.bp[files_n]
   years.nums <- trace.years.nums[files_n]
   file.names <- paste0(indir, vars, "/trace.", years.n, ".", years.bp, ".cam2.h0.", vars, ".", years.nums, ".nc")
   return(file.names)
-} 
+}
 
 #' Title
 #'
@@ -32,32 +32,32 @@ traceFileNames <- function(indir, files_n = 36, vars = NULL){
 #' @param start_date  TBW
 #' @param end_date  TBW
 #' @param years  TBW
-#' @param dictionary TBW 
+#' @param dictionary TBW
 #'
 #' @return TBW
 #' @import loadeR
 #' @export
 #'
-#' @examples 
+#' @examples
 #' # tas <- loadHistoricalTraceGrid(
 #' #            "Data/TraCE21ka/TS/trace.36.400BP-1990CE.cam2.h0.TS.2160101-2204012.nc",
 #' #            "tas")
-loadHistoricalTraceGrid <- function(file, var=NULL, lonLim=c(-25, 25), latLim=c(25, 50), start_date="1551-01-01", end_date="1990-12-31", years=1961:1990, dictionary=system.file("extdata", "TraCE21ka_dictionary.csv", package = "TraCE21kaDSR")) {
+loadHistoricalTraceGrid <- function(file, var=NULL, lonLim=c(-25, 25), latLim=c(25, 50), start_date="1551-01-01", end_date="1990-12-31", years=1961:1990, dictionary=system.file("extdata", "TraCE21ka_dictionary.csv", package = "dsclim")) {
   if(is.null(var)){
     stop("Argument var not defined. Please define a variable to be loaded.")
   }
-  
+
   utils::assignInNamespace("getVerticalLevelPars", getVerticalLevelPars, ns="loadeR")
-  
+
   data <- loadGridData(dataset = file,
                                var = var,
                                lonLim = lonLim,
-                               latLim = latLim, 
+                               latLim = latLim,
                                dictionary = dictionary)
   data <- modifyDates(data, start_date, end_date)
 
-  data <- transformeR::subsetGrid(data, years=years)  
-  
+  data <- transformeR::subsetGrid(data, years=years)
+
   return(data)
 }
 
@@ -80,28 +80,28 @@ loadHistoricalTraceGrid <- function(file, var=NULL, lonLim=c(-25, 25), latLim=c(
 #'
 #' @examples
 #' # historical.files <- traceFileNames(
-#' #                         "Data/TraCE21ka/", 36)  
+#' #                         "Data/TraCE21ka/", 36)
 #' # data <- loadHistoricalTraceGrids(historical.files,
 #' #                                  c("tas", "tasmax", "tasmin", "hurs@992.5561",
-#' #                                  "ps", "pr", "cld", "u@992.5561", "v@992.5561")) 
-loadHistoricalTraceGrids <- function(files, vars = NULL, lonLim=c(-25, 25), latLim=c(25, 50), start_date="1551-01-01", end_date="1990-12-31", years=1961:1990, dictionary=system.file("extdata", "TraCE21ka_dictionary.csv", package = "TraCE21kaDSR"), selection_vars = c("tas", "tasmax", "tasmin", "hurs@992.5561", "ps", "pr", "cld", "wss"), compute_wss=TRUE){
+#' #                                  "ps", "pr", "cld", "u@992.5561", "v@992.5561"))
+loadHistoricalTraceGrids <- function(files, vars = NULL, lonLim=c(-25, 25), latLim=c(25, 50), start_date="1551-01-01", end_date="1990-12-31", years=1961:1990, dictionary=system.file("extdata", "TraCE21ka_dictionary.csv", package = "dsclim"), selection_vars = c("tas", "tasmax", "tasmin", "hurs@992.5561", "ps", "pr", "cld", "wss"), compute_wss=TRUE){
   if(is.null(vars)){
     vars <- trace.standard.var.names
-  } 
+  }
 
   data <- mapply(loadHistoricalTraceGrid, file=files, var=vars, MoreArgs=list(lonLim = lonLim, latLim = latLim, years=years, dictionary=dictionary), SIMPLIFY=FALSE)
-  
+
   names(data) <- vars
-  
+
   if(compute_wss == TRUE){
     data$wss <- compute_wind_speed(data$'u@992.5561', data$'v@992.5561')
-  } 
-  
+  }
+
   data <- transformeR::makeMultiGrid(data)
   data <- transformeR::subsetGrid(data, var=selection_vars)
-  
+
   return(data)
-} 
+}
 
 
 
@@ -120,41 +120,41 @@ loadHistoricalTraceGrids <- function(files, vars = NULL, lonLim=c(-25, 25), latL
 #' @examples
 #' # tas <- loadTraceGrid(
 #' #            "Data/TraCE21ka/TS/trace.01.22000-20001BP.cam2.h0.TS.0000101-0200012.nc",
-#' #            "tas", 1, lonLim = NULL, latLim=NULL) 
-loadTraceGrid <- function(file, var, lonLim = c(-25, 25), latLim = c(25, 50), dictionary=system.file("extdata", "TraCE21ka_dictionary.csv", package = "TraCE21kaDSR")){ 
-  # file <- "../Data/TraCE21ka/TS/trace.01.22000-20001BP.cam2.h0.TS.0000101-0200012.nc"
+#' #            "tas", 1, lonLim = NULL, latLim=NULL)
+loadTraceGrid <- function(file, var, lonLim = c(-25, 25), latLim = c(25, 50), dictionary=system.file("extdata", "TraCE21ka_dictionary.csv", package = "dsclim")){
+  # file <- "Data/TraCE21ka/TS/trace.01.22000-20001BP.cam2.h0.TS.0000101-0200012.nc"
   # var <- "tas"
   # lonLim <- c(-25, 25)
   # latLim <- c(25, 50)
-  # dictionary <- system.file("extdata", "TraCE21ka_dictionary.csv", package = "TraCE21kaDSR")
-  
+  # dictionary <- system.file("extdata", "TraCE21ka_dictionary.csv", package = "dsclim")
+
   file.number <- as.numeric(substr(sub(".*trace.", "", file), 1,2))
-  
-  trace.y1 <- trace.years.y1[file.number] 
-  trace.y2 <- trace.years.y2[file.number] 
-  
+
+  trace.y1 <- trace.years.y1[file.number]
+  trace.y2 <- trace.years.y2[file.number]
+
   var <- loadeR::findVerticalLevel(var)
-  
+
   dic <- dictionaryLookup(dictionary, var$var, "none")
   vocabulary <- climate4R.UDG::C4R.vocabulary()
-  
+
   trace.nc <- ncdf4::nc_open(file)
-  
+
   long_name <- ncdf4::ncatt_get(trace.nc, dic$short_name)$long_name
-  
+
   if(!is.null(dictionary)){
     units <- as.character(vocabulary[grep(paste0("^", var$var, "$"), vocabulary$identifier), 3])
   }else{
     units <- ncdf4::ncatt_get(trace.nc, dic$short_name)$units
-  } 
-  
+  }
+
   trace.c4r <- list()
   trace.c4r[[1]] <- list()
   if(is.null(var$level)){
     trace.c4r[[1]][1]  <- var$var
   }else{
-    trace.c4r[[1]][1]  <- paste0(var$var, "@", var$level) 
-  } 
+    trace.c4r[[1]][1]  <- paste0(var$var, "@", var$level)
+  }
   trace.c4r[[1]][2]   <- list(NULL)
   names(trace.c4r[[1]]) <- c("varName", "level")
   attr(trace.c4r[[1]], "use_dictionary") <- TRUE
@@ -164,37 +164,37 @@ loadTraceGrid <- function(file, var, lonLim = c(-25, 25), latLim = c(25, 50), di
   attr(trace.c4r[[1]], "daily_agg_cellfun") <- "none"
   attr(trace.c4r[[1]], "monthly_agg_cellfun") <- "none"
   attr(trace.c4r[[1]], "verification_time") <- "none"
-  
+
   if(!is.null(var$level)){
     lev <- ncdf4::ncvar_get(trace.nc, "lev")
     lev <- which(.elementwise.all.equal(lev, var$level))
     trace.var <- ncdf4::ncvar_get(trace.nc, dic$short_name, c(1,1,lev,1))
   }else{
     trace.var <- ncdf4::ncvar_get(trace.nc, dic$short_name)
-  }  
-  
+  }
+
   trace.c4r[[2]] <- aperm(trace.var, c(3,2,1)) * dic$scale + dic$offset
-  
+
   trace.c4r[[3]] <- list()
   # trace.c4r[[3]]$x <- ncvar_get(trace.nc, "lon")
   lon <- ncdf4::ncvar_get(trace.nc, "lon")
-  lon[which(lon>180)] <- lon[which(lon>180)] - 360 
+  lon[which(lon>180)] <- lon[which(lon>180)] - 360
   trace.c4r[[3]]$x <- lon
   trace.c4r[[3]]$y <- ncdf4::ncvar_get(trace.nc, "lat")
   attr(trace.c4r[[3]], "projection") <- "LatLonProjection"
   attr(trace.c4r[[3]], "resX") <- (max(trace.c4r[[3]]$x) - min(trace.c4r[[3]]$x)) / (length(trace.c4r[[3]]$x) - 1)
   attr(trace.c4r[[3]], "resY") <- (max(trace.c4r[[3]]$y) - min(trace.c4r[[3]]$y)) / (length(trace.c4r[[3]]$y) - 1)
-  
+
   trace.c4r[[2]] <- trace.c4r[[2]][,,order(trace.c4r[[3]]$x)]
   trace.c4r[[3]]$x <- sort(trace.c4r[[3]]$x)
   attr(trace.c4r[[2]], "dimensions") <- c("time", "lat", "lon")
-  
+
   if(0 %in% trace.y1:trace.y2){
-    n.years <- length(trace.y1:trace.y2) - 1    
+    n.years <- length(trace.y1:trace.y2) - 1
   }else{
     n.years <- length(trace.y1:trace.y2)
-  }  
-  trace.c4r[[4]] <- list() 
+  }
+  trace.c4r[[4]] <- list()
   trace.c4r[[4]]$start <- paste(.DateSeq("4000-01-01", paste0(3999+n.years, "-12-31"), 12, 0), "00:00:00 GMT", sep = " ")
   trace.c4r[[4]]$end <- paste(.DateSeq("4000-01-01", paste0(3999+n.years, "-12-31"), 12, 1), "00:00:00 GMT", sep = " ")
   # y1 <- lubridate::ymd("1950-01-01") - lubridate::years(trace.y1)
@@ -202,20 +202,20 @@ loadTraceGrid <- function(file, var, lonLim = c(-25, 25), latLim = c(25, 50), di
   # trace.c4r[[4]]$start <- format(add_with_rollback(y1, months(x = seq.int(from = 0, to = (n.years * 12) - 1, by = 1))), "%Y-%m-%d %H:%M:%S %Z")
   # trace.c4r[[4]]$end <- format(add_with_rollback(y1 + days(30), months(x = seq.int(from = 0, to = (n.years * 12) - 1, by = 1))), "%Y-%m-%d %H:%M:%S %Z")
   attr(trace.c4r[[4]], "subset") <- "subsetYears"
-  attr(trace.c4r[[4]], "season") <- 1:12 
-  
+  attr(trace.c4r[[4]], "season") <- 1:12
+
   names(trace.c4r) <- c("Variable", "Data", "xyCoords", "Dates")
-  
+
   attr(trace.c4r, "dataset") <- file
   attr(trace.c4r, "R_package_desc") <- paste0("loadeR-v", utils::packageVersion("loadeR"))
   attr(trace.c4r, "R_package_URL") <- "https://github.com/SantanderMetGroup/loadeR"
   attr(trace.c4r, "R_package_ref") <- "https://doi.org/10.1016/j.envsoft.2018.09.009"
-  
+
   if(!is.null(lonLim) & !is.null(latLim)){
     trace.c4r <- transformeR::subsetGrid(trace.c4r, lonLim = lonLim, latLim = latLim)
-  } 
+  }
   trace.c4r <- recalcGridResolution(trace.c4r)
-  
+
   return(trace.c4r)
 }
 
@@ -235,38 +235,38 @@ loadTraceGrid <- function(file, var, lonLim = c(-25, 25), latLim = c(25, 50), di
 #'
 #' @examples
 #' # LGM.files <- traceFileNames(
-#' #                         "../Data/TraCE21ka/", 1)  
+#' #                         "Data/TraCE21ka/", 1)
 #' # data <- loadTraceGrids(LGM.files,
 #' #                                  c("tas", "tasmax", "tasmin", "hurs@992.5561",
 #' #                                  "ps", "pr", "cld", "u@992.5561", "v@992.5561"), 1,
 #' #                                  selection_vars = c("tas", "tasmax", "tasmin",
 #' #                                                  "hurs@992.5561", "ps", "pr",
 #' #                                                  "cld", "wss"))
-loadTraceGrids <- function(files, vars = NULL, lonLim = c(-25, 25), latLim = c(25, 50), dictionary=system.file("extdata", "TraCE21ka_dictionary.csv", package = "TraCE21kaDSR"), selection_vars = c("tas", "tasmax", "tasmin", "hurs@992.5561", "ps", "pr", "cld", "wss"), compute_wss=TRUE){
-  
+loadTraceGrids <- function(files, vars = NULL, lonLim = c(-25, 25), latLim = c(25, 50), dictionary=system.file("extdata", "TraCE21ka_dictionary.csv", package = "dsclim"), selection_vars = c("tas", "tasmax", "tasmin", "hurs@992.5561", "ps", "pr", "cld", "wss"), compute_wss=TRUE){
+
   if(is.null(vars)){
     vars <- trace.standard.var.names
-  } 
-  
+  }
+
   data <- mapply(loadTraceGrid, file = files, var = vars, MoreArgs = list(lonLim, latLim, dictionary), SIMPLIFY = FALSE)
-  
+
   names(data) <- vars
-  
+
   if(compute_wss == TRUE){
     data$wss <- compute_wind_speed(data$'u@992.5561', data$'v@992.5561')
-  } 
-  
+  }
+
   data <- transformeR::makeMultiGrid(data)
   data <- transformeR::subsetGrid(data, var=selection_vars)
-  
+
   return(data)
-} 
+}
 
 
 #' Title
 #'
 #' @param files_n TBW
-#' @param outdir TBW  
+#' @param outdir TBW
 #' @param trace_dir TBW
 #' @param vars TBW
 #' @param lonLim TBW
@@ -283,8 +283,8 @@ loadTraceGrids <- function(files, vars = NULL, lonLim = c(-25, 25), latLim = c(2
 #' @examples #TBW
 downscaleTrace <- function(files_n, outdir, trace_dir, vars = NULL, lonLim = c(-25, 25), latLim = c(25, 50), hist_trace, mod_data, model, selection_vars = NULL, global_nc_attributes = NULL){
   # files_n <- 1
-  # outdir <- "../Output/Trace21ka/"
-  # trace_dir <- "../Data/TraCE21ka/"
+  # outdir <- "Output/Trace21ka/"
+  # trace_dir <- "Data/TraCE21ka/"
   # vars <- NULL
   # lonLim <- c(-25, 25)
   # latLim <- c(25, 50)
@@ -293,64 +293,64 @@ downscaleTrace <- function(files_n, outdir, trace_dir, vars = NULL, lonLim = c(-
   # model <- model
   # selection_vars <- NULL
   # global_nc_attributes <- global.nc.attributes
-  
+
   y.var <- mod_data$y$Variable$varName
-  
+
   if(!dir.exists(paste0(outdir, y.var, "/dat"))){
     dir.create(paste0(outdir, y.var, "/dat"), recursive = TRUE)
-  } 
-  
+  }
+
   if(is.null(selection_vars)){
     selection_vars <- c("tas", "tasmax", "tasmin", "hurs@992.5561", "ps", "pr", "cld", "wss")
-  } 
-  
+  }
+
   new.data <- traceFileNames(trace_dir, files_n)
 
   new.trace <- loadTraceGrids(new.data, vars, lonLim, latLim, selection_vars = selection_vars)
-  
+
   new.trace.xy <- copyXYCoords(new.trace, hist_trace)
-  
-  y1 <- trace.years.y1[[files_n]] 
-  y2 <- trace.years.y2[[files_n]] 
+
+  y1 <- trace.years.y1[[files_n]]
+  y2 <- trace.years.y2[[files_n]]
   if(y1 == 400){
     real.years <- c(-y1:-1,1:-y2)
   }else{
     real.years <- -y1:-y2
-  }  
-  
+  }
+
   fake.years <- seq(4000, length=length(real.years))
-  
-  ydiff <- -y1 - 3999  
-  
+
+  ydiff <- -y1 - 3999
+
   for(j in 1:length(fake.years)){
-    
+
     message("Calculating year: ", real.years[j], "...")
-    
+
     new.trace.sub <- transformeR::subsetGrid(new.trace.xy, years = fake.years[j])
 
     new.data <- downscaleR::prepareNewData(new.trace.sub, mod_data)
-    
+
     pred <- downscaleR::downscalePredict(new.data, model)
-    
+
     pred$Data <- round(pred$Data, 2)
-    
+
     loadeR.2nc::grid2nc(pred, NetCDFOutFile = paste0(outdir, y.var, "/", y.var, real.years[j], "_tmp.nc"), missval = -9999, globalAttributes = global_nc_attributes)
-    
+
     infile <- paste0(outdir, y.var, "/", y.var, real.years[j],  "_tmp.nc")
     outfile <- paste0(outdir, y.var, "/", y.var, real.years[j], ".nc")
-    
-    # system(paste0("cdo -r setreftime,1950-01-01,00:00:00,hours -shifttime,", ydiff, "y ", infile, " ", outfile)) #not working  
+
+    # system(paste0("cdo -r setreftime,1950-01-01,00:00:00,hours -shifttime,", ydiff, "y ", infile, " ", outfile)) #not working
     system2("cdo", c("-r", "setreftime,1950-01-01,00:00:00,1mon", "-setcalendar,standard", paste0("-shifttime,", ydiff, "y"), infile, outfile))
     file.remove(infile)
-    
+
     pred.df <- nc2sp_df(pred)
-    
+
     utils::write.table(pred.df, file=paste0(outdir, y.var, "/dat/", y.var, real.years[j], ".dat"), sep="\t")
-    
+
     message("   ...done")
-  } 
+  }
   return("Done")
-} 
+}
 
 
 #' Title
@@ -376,10 +376,10 @@ downscaleTrace <- function(files_n, outdir, trace_dir, vars = NULL, lonLim = c(-
 #'
 #' @examples #TBW
 downscaleTraceBimodel <- function (i, new.data.list, vars, y1.list, y2.list, lonLim, latLim, hist.trace, data, model, data.bin, model.bin, local.var, trace_dir, global.nc.attributes){
-  
+
   # i <- 36
   # new.data.list <- new.trace.file.names
-  # vars <- TraCE21kaDSR:::trace.standard.var.names
+  # vars <- dsclim:::trace.standard.var.names
   # y1.list <- trace.years.y1
   # y2.list <- trace.years.y2
   # lonLim <- trace.lon
@@ -390,68 +390,68 @@ downscaleTraceBimodel <- function (i, new.data.list, vars, y1.list, y2.list, lon
   # data.bin <- data_bin
   # model.bin <- model_bin
   # local.var <- local.var
-  # trace_dir <- TraCE21kaDSR:::trace.final.var.names
+  # trace_dir <- dsclim:::trace.final.var.names
   # global.nc.attributes <- global.nc.attributes
-  
-  if(!dir.exists(paste0("../../Output/Trace21ka/", local.var, "/dat"))){
-    dir.create(paste0("../../Output/Trace21ka/", local.var, "/dat"))
-  } 
-  
-  new.data <- new.data.list[[i]] 
-  y1 <- y1.list[[i]] 
-  y2 <- y2.list[[i]] 
-  
+
+  if(!dir.exists(paste0("Output/Trace21ka/", local.var, "/dat"))){
+    dir.create(paste0("Output/Trace21ka/", local.var, "/dat"))
+  }
+
+  new.data <- new.data.list[[i]]
+  y1 <- y1.list[[i]]
+  y2 <- y2.list[[i]]
+
   new.trace <- loadTraceGrids(new.data, vars, y1, y2, lonLim, latLim, selection_vars = trace_dir)
-  
+
   new.trace.xy <- copyXYCoords(new.trace, hist.trace)
-  
+
   if(y1 == 400){
     real.years <- c(-y1:-1,1:-y2)
   }else{
     real.years <- -y1:-y2
-  }  
-  
+  }
+
   fake.years <- seq(4000, length=length(real.years))
-  
+
   for(j in 1:length(fake.years)){
-    
+
     if(real.years[j] > 0){
-      ydiff <- -y1 - 4000  
+      ydiff <- -y1 - 4000
     }else{
       ydiff <- -y1 - 3999
-    } 
-    
+    }
+
     message("Calculating year: ", real.years[j], "...")
-    
+
     new.trace.sub <- transformeR::subsetGrid(new.trace.xy, years = fake.years[j])
-    
+
     new.data <- downscaleR::prepareNewData(new.trace.sub, data)
-    
+
     pred.bin <- downscaleR::downscalePredict(new.data, model.bin)
     pred.cont <- downscaleR::downscalePredict(new.data, model)
-    
+
     pred <- transformeR::gridArithmetics(pred.bin, pred.cont, operator = "*")
-    
+
     pred$Data <- round(pred$Data, 2)
-    
-    loadeR.2nc::grid2nc(pred, NetCDFOutFile = paste0("../../Output/Trace21ka/", local.var, "/", local.var, real.years[j], "_tmp.nc"), missval = -9999, globalAttributes = global.nc.attributes)
-    
-    infile <- paste0("../../Output/Trace21ka/", local.var, "/", local.var, real.years[j],  "_tmp.nc")
-    outfile <- paste0("../../Output/Trace21ka/", local.var, "/", local.var, real.years[j], ".nc")
-    
+
+    loadeR.2nc::grid2nc(pred, NetCDFOutFile = paste0("Output/Trace21ka/", local.var, "/", local.var, real.years[j], "_tmp.nc"), missval = -9999, globalAttributes = global.nc.attributes)
+
+    infile <- paste0("Output/Trace21ka/", local.var, "/", local.var, real.years[j],  "_tmp.nc")
+    outfile <- paste0("Output/Trace21ka/", local.var, "/", local.var, real.years[j], ".nc")
+
     message("New year: ", fake.years[[j]] + ydiff)
-    
+
     # system(paste0("cdo -r setreftime,1950-01-01,00:00:00,hours -shifttime,", ydiff, "y ", infile, " ", outfile))
     system2("cdo", c("-r", "setreftime,1950-01-01,00:00:00,1mon", "-setcalendar,standard", paste0("-shifttime,", ydiff, "y"), infile, outfile))
     file.remove(infile)
-    
+
     pred.df <- nc2sp_df(pred)
-    
-    utils::write.table(pred.df, file=paste0("../../Output/Trace21ka/", local.var, "/dat/", local.var, real.years[j], ".dat"), sep="\t")
-    
+
+    utils::write.table(pred.df, file=paste0("Output/Trace21ka/", local.var, "/dat/", local.var, real.years[j], ".dat"), sep="\t")
+
     message("   ...done")
-  } 
+  }
   return("Done")
-} 
+}
 
 
